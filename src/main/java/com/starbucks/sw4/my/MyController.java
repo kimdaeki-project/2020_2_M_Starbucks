22ee9e7525b8ece0609f5bdf02e1648cf7ed0791
JiyoonChae <71802752+JiyoonChae@users.noreply.github.com>
2020-11-16 17:06:30 +0900

    Create findMyInfo jsp

---
+++ src/main/java/com/starbucks/sw4/my/MyController.java
@@ -0,0 +1,15 @@
+package com.starbucks.sw4.my;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/my/**")
+public class MyController {
+	
+	@GetMapping("findMyInfo")
+	public void findMyInfo() {
+		
+	}
+}
