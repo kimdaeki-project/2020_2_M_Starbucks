9ae256f4a21dff00e6c13702277f54492f642086
minju5467 <71800775+minju5467@users.noreply.github.com>
2020-11-25 17:57:27 +0900

    201125 1757

--- src/main/java/com/starbucks/sw4/product/ProductDTO.java
+++ src/main/java/com/starbucks/sw4/product/ProductDTO.java
@@ -2,4 +2,251 @@ package com.starbucks.sw4.product;
 
 public class ProductDTO {
 
+	private String productCode;
+	private String category;
+	private String theme;
+	private String starbucksBean;
+	private String starbucksVia;
+	private String starbucksOrigami;
+	private String productFName;
+	private String productSName;
+	private String defOption;
+	private String addOption;
+	private String pNew;
+	private String sold;
+	private int quantity;
+	private int price;
+	private int totalPrice;
+	private String allergy;
+	private int kal;
+	private int sugar;
+	private int protein;
+	private int natrium;
+	private int fat;
+	private String season;
+	private String themeCode;
+	private String sidoCode;
+	private int gugunCode;
+	private String limStoreCode;
+	private long storeCode;
+	private String description;
+	private String addDescription;
+	private String rstDescription;
+	private String tastingNotes;
+	private String enjoyWith;
+	private String origin;
+	private String productFile;
+	private String originFile;
+	
+	public String getProductCode() {
+		return productCode;
+	}
+	public void setProductCode(String productCode) {
+		this.productCode = productCode;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getTheme() {
+		return theme;
+	}
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+	public String getStarbucksBean() {
+		return starbucksBean;
+	}
+	public void setStarbucksBean(String starbucksBean) {
+		this.starbucksBean = starbucksBean;
+	}
+	public String getStarbucksVia() {
+		return starbucksVia;
+	}
+	public void setStarbucksVia(String starbucksVia) {
+		this.starbucksVia = starbucksVia;
+	}
+	public String getStarbucksOrigami() {
+		return starbucksOrigami;
+	}
+	public void setStarbucksOrigami(String starbucksOrigami) {
+		this.starbucksOrigami = starbucksOrigami;
+	}
+	public String getProductFName() {
+		return productFName;
+	}
+	public void setProductFName(String productFName) {
+		this.productFName = productFName;
+	}
+	public String getProductSName() {
+		return productSName;
+	}
+	public void setProductSName(String productSName) {
+		this.productSName = productSName;
+	}
+	public String getDefOption() {
+		return defOption;
+	}
+	public void setDefOption(String defOption) {
+		this.defOption = defOption;
+	}
+	public String getAddOption() {
+		return addOption;
+	}
+	public void setAddOption(String addOption) {
+		this.addOption = addOption;
+	}
+	public String getpNew() {
+		return pNew;
+	}
+	public void setpNew(String pNew) {
+		this.pNew = pNew;
+	}
+	public String getSold() {
+		return sold;
+	}
+	public void setSold(String sold) {
+		this.sold = sold;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	public String getAllergy() {
+		return allergy;
+	}
+	public void setAllergy(String allergy) {
+		this.allergy = allergy;
+	}
+	public int getKal() {
+		return kal;
+	}
+	public void setKal(int kal) {
+		this.kal = kal;
+	}
+	public int getSugar() {
+		return sugar;
+	}
+	public void setSugar(int sugar) {
+		this.sugar = sugar;
+	}
+	public int getProtein() {
+		return protein;
+	}
+	public void setProtein(int protein) {
+		this.protein = protein;
+	}
+	public int getNatrium() {
+		return natrium;
+	}
+	public void setNatrium(int natrium) {
+		this.natrium = natrium;
+	}
+	public int getFat() {
+		return fat;
+	}
+	public void setFat(int fat) {
+		this.fat = fat;
+	}
+	public String getSeason() {
+		return season;
+	}
+	public void setSeason(String season) {
+		this.season = season;
+	}
+	public String getThemeCode() {
+		return themeCode;
+	}
+	public void setThemeCode(String themeCode) {
+		this.themeCode = themeCode;
+	}
+	public String getSidoCode() {
+		return sidoCode;
+	}
+	public void setSidoCode(String sidoCode) {
+		this.sidoCode = sidoCode;
+	}
+	public int getGugunCode() {
+		return gugunCode;
+	}
+	public void setGugunCode(int gugunCode) {
+		this.gugunCode = gugunCode;
+	}
+	public String getLimStoreCode() {
+		return limStoreCode;
+	}
+	public void setLimStoreCode(String limStoreCode) {
+		this.limStoreCode = limStoreCode;
+	}
+	public long getStoreCode() {
+		return storeCode;
+	}
+	public void setStoreCode(long storeCode) {
+		this.storeCode = storeCode;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getAddDescription() {
+		return addDescription;
+	}
+	public void setAddDescription(String addDescription) {
+		this.addDescription = addDescription;
+	}
+	public String getRstDescription() {
+		return rstDescription;
+	}
+	public void setRstDescription(String rstDescription) {
+		this.rstDescription = rstDescription;
+	}
+	public String getTastingNotes() {
+		return tastingNotes;
+	}
+	public void setTastingNotes(String tastingNotes) {
+		this.tastingNotes = tastingNotes;
+	}
+	public String getEnjoyWith() {
+		return enjoyWith;
+	}
+	public void setEnjoyWith(String enjoyWith) {
+		this.enjoyWith = enjoyWith;
+	}
+	public String getOrigin() {
+		return origin;
+	}
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+	public String getProductFile() {
+		return productFile;
+	}
+	public void setProductFile(String productFile) {
+		this.productFile = productFile;
+	}
+	public String getOriginFile() {
+		return originFile;
+	}
+	public void setOriginFile(String originFile) {
+		this.originFile = originFile;
+	}
+	
 }
