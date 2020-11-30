d147570824dd2b7986e42e7e1ef8e517a0cce207
CSM <tbehd8967@naver.com>
2020-11-30 20:53:55 +0900

    Revert "[JeongSky] 20201130 1743"
    This reverts commit 1af3189d0a69af24f5a64411d583b26e3f3587e1.

--- src/main/java/com/starbucks/sw4/menu/MenuController.java
+++ src/main/java/com/starbucks/sw4/menu/MenuController.java
@@ -43,6 +43,8 @@ public class MenuController {
 		
 		mv.addObject("categoryList", categoryList);
 		mv.addObject("list", menuList);
+		//mv.setViewName("menu/productList");
+		//하늘 테스트
 		mv.setViewName("menu/tmp_productList");
 		
 		return mv;
@@ -66,6 +68,8 @@ public class MenuController {
 
 		mv.addObject("categoryList", categoryList);
 		mv.addObject("list", menuList);
+		//mv.setViewName("menu/productList");
+		//하늘 테스트
 		mv.setViewName("menu/tmp_productList");
 
 		System.out.println(menuList);
@@ -91,6 +95,8 @@ public class MenuController {
 
 		mv.addObject("categoryList", categoryList);
 		mv.addObject("list", menuList);
+		//mv.setViewName("menu/productList");
+		//하늘 테스트
 		mv.setViewName("menu/tmp_productList");
 		
 		System.out.println(menuList);
